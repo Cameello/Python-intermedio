c3475adbec9da8c1aa1e1e64f99dc0901747620b
Cameello <martin.n.moreno@outlook.com>
2025-08-31 17:01:21 -0300

    agrego practica1.py

---
+++ practica1.py
@@ -0,0 +1,6 @@
+# Dados dos conjuntos, A y B, escribe un programa en Python que imprima los elementos que se encuentran en A o en B, o en ambos.
+# Dados dos conjuntos, A y B, escribe un programa en Python que imprima loselementos que se encuentran en A y en B
+# Dados dos conjuntos, A y B, escribe un programa en Python que imprima elconjunto de los elementos que se encuentran en A o en B, pero no en ambos.
+# Dados un conjunto, A, escribe un programa en Python que imprima si el conjunto esun subconjunto de otro conjunto, B.
+# Dados un conjunto, A, escribe un programa en Python que imprima el número de elementos del conjunto
+
